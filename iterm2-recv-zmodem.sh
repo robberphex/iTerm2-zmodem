638822532d9dc9532862046d3e06404da2383fd5
Matt Mastracci <matthew@mastracci.com>
2011-10-26 16:38:24 -0600

    Support receiving as well

---
+++ iterm2-recv-zmodem.sh
@@ -0,0 +1,19 @@
+# Author: Matt Mastracci (matthew@mastracci.com)
+# AppleScript from http://stackoverflow.com/questions/4309087/cancel-button-on-osascript-in-a-bash-script
+# licensed under cc-wiki with attribution required 
+# Remainder of script public domain
+
+FILE=`osascript -e 'tell application "iTerm" to activate' -e 'tell application "iTerm" to set thefile to choose folder with prompt "Choose a folder to place received files in"' -e "do shell script (\"echo \"&(quoted form of POSIX path of thefile as Unicode text)&\"\")"`
+if [[ $FILE = "" ]]; then
+	echo Cancelled.
+	# Send ZModem cancel
+	echo -e \\x18\\x18\\x18\\x18\\x18
+	echo \# Cancelled transfer
+	echo
+else
+	echo $FILE
+	cd "$FILE"
+	/usr/local/bin/rz 
+	echo \# Received $FILE
+	echo
+fi
